f635f4741ead4d7ad67d5d0f9c652c7353ceae21
Ovilov <ovilovsaidakbar@gmail.com>
2024-04-11 22:00:11 +0500

    Age calc add

---
+++ js/app.js
@@ -0,0 +1,69 @@
+const dayIn = document.querySelector(".day");
+const monthIn = document.querySelector(".month");
+const yearIn = document.querySelector(".year");
+
+const dd = document.querySelector(".DD");
+const mm = document.querySelector(".MM");
+const yy = document.querySelector(".YY");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+const date = new Date();
+
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function validate() {
+  const inputs = document.querySelectorAll("input");
+  let validator = true;
+  inputs.forEach((e) => {
+    const parent = e.parentElement;
+    if (!e.value) {
+      
+      e.style.borderColor = "red";
+      parent.querySelector("small").innerText = "This field is required";
+      validator = false;
+    } else if (monthIn.value > 12) {
+        console.log(1);
+      monthIn.style.borderColor = "red";
+      monthIn.querySelector("small").innerText = "Must be a valid month";
+      validator = false;
+    } else if (dayIn > 31) {
+      dayIn.style.borderColor = "red";
+      dayIn.querySelector("small").innerText = "Must be a valid day";
+      validator = false;
+    } else {
+      e.style.borderColor = "black";
+      parent.querySelector("small").innerText = "";
+      validator = true;
+    }
+  });
+  return validator;
+}
+
+function handleSubmit(i) {
+  i.preventDefault();
+  if (validate()) {
+    if (dayIn.value > day) {
+      day = day + months[month - 1];
+      month = month - 1;
+    }
+    if (monthIn.value > month) {
+      month = month + 12;
+      year = year - 1;
+    }
+
+    const d = day - dayIn.value;
+    const m = month - monthIn.value;
+    const y = year - yearIn.value;
+
+    dd.innerHTML = d;
+    mm.innerHTML = m;
+    yy.innerHTML = y;
+  }
+}
